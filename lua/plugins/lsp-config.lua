04af0720afdfb099b874a3a033e84861f0a346c3
MannuVilasara <mannuvilasara@gmail.com>
2025-10-06 16:57:52 +0530

    🐛 fix: removed depricated require(lspconfig)

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -25,25 +25,60 @@ return {
 				cmp_nvim_lsp.default_capabilities()
 			)
 
-			local lspconfig = require("lspconfig")
+			-- Using the new vim.lsp.config API (Neovim 0.11+)
+			-- Configure TailwindCSS LSP
+			vim.lsp.config.tailwindcss = {
+				cmd = { "tailwindcss-language-server", "--stdio" },
+				filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte" },
+				root_markers = { "tailwind.config.js", "tailwind.config.cjs", "tailwind.config.mjs", "tailwind.config.ts" },
+				capabilities = capabilities,
+			}
 
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ruby_lsp.setup({
-				capabilities = capabilities,
+			-- Configure Ruby LSP
+			vim.lsp.config.ruby_lsp = {
 				cmd = { "/home/typecraft/.asdf/shims/ruby-lsp" },
-			})
-			lspconfig.lua_ls.setup({
+				filetypes = { "ruby", "eruby" },
+				root_markers = { "Gemfile", ".git" },
 				capabilities = capabilities,
-			})
+			}
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+			-- Configure Lua LS
+			vim.lsp.config.lua_ls = {
+				cmd = { "lua-language-server" },
+				filetypes = { "lua" },
+				root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			}
+
+			-- Enable LSP servers
+			vim.lsp.enable("tailwindcss")
+			vim.lsp.enable("ruby_lsp")
+			vim.lsp.enable("lua_ls")
+
+			-- LSP Keybindings
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover documentation" })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "LSP: Go to definition" })
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP: Find references" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code action" })
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "LSP: Format" })
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { desc = "LSP: Rename" })
 		end,
 	},
 }
