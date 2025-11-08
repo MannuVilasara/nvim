add14e372eadcea62845d2ffd74152fdad673e17
MannuVilasara <mannuvilasara@gmail.com>
2025-11-08 12:06:57 +0530

    commandline box

---
+++ lua/plugins/ui/noice.lua
@@ -0,0 +1,62 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	opts = {
+		-- you can enable a preset for easier configuration
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			command_palette = true, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			lsp_doc_border = false, -- add a border to hover docs and signature help
+		},
+		cmdline = {
+			enabled = true,
+			view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+			opts = {
+				position = {
+					row = "50%", -- center vertically
+					col = "50%", -- center horizontally
+				},
+			},
+			format = {
+				cmdline = { pattern = "^:", icon = "", lang = "vim" },
+				search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+				search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+				filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+				lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+				help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖" },
+				input = {},
+			},
+		},
+		messages = {
+			enabled = false, -- disable messages UI
+		},
+		lsp = {
+			progress = {
+				enabled = false,
+			},
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+				["vim.lsp.util.stylize_markdown"] = false,
+				["cmp.entry.get_documentation"] = false,
+			},
+			hover = {
+				enabled = false,
+			},
+			signature = {
+				enabled = false,
+			},
+			message = {
+				enabled = false,
+			},
+		},
+		popupmenu = {
+			enabled = true, -- enables the Noice popupmenu UI
+			backend = "nui", -- backend to use to show regular cmdline completions
+		},
+	},
+}
