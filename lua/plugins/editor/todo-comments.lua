29d47a8e09a9c3acd5f63ab4e64647092fa812f5
MannuVilasara <mannuvilasara@gmail.com>
2025-11-09 20:39:32 +0530

    feat: todo-comments

---
+++ lua/plugins/editor/todo-comments.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {},
+}
+
+-- EXAMPLE COMMENTS:
+-- TODO: This is a todo comment
+-- FIX: This is a fix comment
+-- HACK: This is a hack comment
+-- NOTE: This is a note comment
+-- WARNING: This is a warn comment
+-- PERF: This is a perf comment
+-- TEST: This is a test comment
